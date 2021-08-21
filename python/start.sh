25cb137e4c7bbf5b1449e79c12f4f9b4cce9a539
denzow <denzow@gmail.com>
2021-08-21 12:22:33 +0900

    [change] source

--- python/start.sh
+++ python/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-export MYSQL_HOST="127.0.0.1"
-export MYSQL_PORT=3306
-export MYSQL_USER=isucon
-export MYSQL_DBNAME=isucondition
-export MYSQL_PASS=isucon
-export JIA_JWT_SIGNING_KEY_PATH=
-export POST_ISUCONDITION_TARGET_BASE_URL="https://isucondition-1.t.isucon.dev"
+source /home/isucon/env.sh
 export APP_ROUTE="/home/isucon/webapp/"
 /home/isucon/local/python/bin/ddtrace-run /home/isucon/local/python/bin/uwsgi --ini /home/isucon/webapp/python/uwsgi.ini
